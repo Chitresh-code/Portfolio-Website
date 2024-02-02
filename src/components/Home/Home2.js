e246643771197da1af63dee5614fbe8db752d7ca
Chitresh Gyanani <129378666+Chitresh-code@users.noreply.github.com>
2024-02-03 00:56:16 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,7 +24,7 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> Python, Django and C++. </b>
+                <b className="purple"> Python, Javascript and C++. </b>
               </i>
               <br />
               <br />
